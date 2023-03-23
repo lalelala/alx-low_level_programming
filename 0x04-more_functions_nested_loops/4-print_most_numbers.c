52d40434173aceb5ee7898dbbc6942a81acd8af6
lalelala <theunknownpiper@gmail.com>
2023-03-23 09:24:43 -0700

    k

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,13 +6,15 @@
  */
 void print_most_numbers(void)
 {
-	int i = 0;
-	for ( i = 0; i <10; i++)
+	int i;
+
+	for (i = 0; i < 10; i++)
 	{
-		if ( i != 2 && i != 4)
+		if (i != 2 && i != 4)
 		{
-			_puthchar( i + 0');
+			_putchar(i + '0');
 		}
 	}
-	_putchar('/n');
+
+	_putchar('\n');
 }
