4458fe58a51a3d158745885d761e2e11284aa827
lalelala <theunknownpiper@gmail.com>
2023-04-06 14:40:19 -0700

    betty sucks

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * power_op - returns n squared by c.
+ * @n: input.
+ * @c: iterator.
+ * Return: the sqaur root or -1 if no square.
+ */
+int power_op(int n, int c)
+{
+	if (c % (n / c) == 0)
+		return (-1);
+	else if (c * (n / c) == n)
+		return (c);
+	else
+		return (0 + power_op(n, c + 1));
+}
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
+ * @n: input number.
+ * Return: natural square root.
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (0);
+	else if (n == 1)
+		return (1);
+	else
+		return (power_op(n, 2));
+}
