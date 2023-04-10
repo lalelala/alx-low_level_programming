c58655f160bf37e16c7deb509052e6d78aaa5a5f
lalelala <theunknownpiper@gmail.com>
2023-04-10 10:59:43 -0700

    hate this

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,11 +6,11 @@
   * @argc: no of arguments.
   * @argv: contains command line argument.
   *
-  * Return: 0 if successful.
+  * return: 0.
   */
-
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	printf("%s\n", *argv[]);
+	printf("%s\n", argv[0]);
+
 	return (0);
 }
