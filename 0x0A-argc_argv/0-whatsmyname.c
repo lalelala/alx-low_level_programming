2f69db5844a7413a6ae15548a2ea3f15b1271d84
lalelala <theunknownpiper@gmail.com>
2023-04-10 10:55:47 -0700

    over this

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 /**
@@ -10,6 +11,6 @@
 
 int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s\n", *argv[0]);
+	printf("%s\n", *argv[]);
 	return (0);
 }
